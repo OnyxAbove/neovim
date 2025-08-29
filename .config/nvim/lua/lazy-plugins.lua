5ee97ce87277d20f6c75b50cfcd48587c26f77cd
Roshan Studley <iluminated.horse@gmail.com>
2025-08-28 17:33:18 -0700

    refacor configuration

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -25,48 +25,45 @@ require('lazy').setup({
 
   --  require 'plugins/*.lua',
 
-  require 'kickstart/plugins/gitsigns',
+  require 'rstudley.plugins.gitsigns',
 
-  require 'kickstart/plugins/which-key',
+  require 'rstudley.plugins.which-key',
 
-  require 'kickstart/plugins/telescope',
+  require 'rstudley.plugins.telescope',
 
-  require 'kickstart/plugins/lspconfig',
+  require 'rstudley.plugins.lspconfig',
 
-  require 'kickstart/plugins/conform',
+  require 'rstudley.plugins.conform',
 
-  require 'kickstart/plugins/cmp',
+  require 'rstudley.plugins.cmp',
 
-  require 'kickstart/plugins/tokyonight',
+  require 'rstudley.plugins.tokyonight',
 
-  require 'kickstart/plugins/todo-comments',
+  require 'rstudley.plugins.todo-comments',
 
-  require 'kickstart/plugins/mini',
+  require 'rstudley.plugins.mini',
 
-  require 'kickstart/plugins/treesitter',
+  require 'rstudley.plugins.treesitter',
 
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
+  require 'rstudley.plugins.neo-tree',
 
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
+  require 'rstudley.plugins.bufferline',
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  { import = 'custom.plugins' },
+  require 'rstudley.plugins.fzf',
+
+  require 'rstudley.plugins.rust',
+
+  require 'rstudley.plugins.catppuccin',
+
+  -- require 'rstudley.plugins.debug',
+
+  -- require 'rstudley.plugins.indent_line',
+
+  -- require 'rstudley.plugins.lint',
+
+  -- require 'rstudley.plugins.autopairs',
 }, {
+
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
